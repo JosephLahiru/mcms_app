e17a2c1f2550714dc350cfce3c4391e51c39c037
INFINITY <nirajnimantha@gmail.com>
2023-01-22 14:54:24 +0530

    basic-setup

--- lib/screens/inventory_details.dart
+++ lib/screens/inventory_details.dart
@@ -5,6 +5,51 @@ class InventoryDetails extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final data = List.generate(
+      10,
+          (index) => InventoryData(
+        'Item $index',
+        'Description $index',
+        (index + 1) * 10,
+        (index + 1) * 20,
+      ),
+    );
+
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('Inventory'),
+      ),
+      body: Center(
+        child: DataTable(
+          columns: [
+            DataColumn(label: Text('Item')),
+            DataColumn(label: Text('Description')),
+            DataColumn(label: Text('Quantity')),
+            DataColumn(label: Text('Price')),
+          ],
+          rows: List.generate(
+            data.length,
+                (index) => DataRow(
+              cells: [
+                DataCell(Text(data[index].item)),
+                DataCell(Text(data[index].description)),
+                DataCell(Text(data[index].quantity.toString())),
+                DataCell(Text(data[index].price.toString())),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
+}
+
+class InventoryData {
+  final String item;
+  final String description;
+  final int quantity;
+  final int price;
+
+  InventoryData(this.item, this.description, this.quantity, this.price);
 }
--- lib/screens/reports.dart
+++ lib/screens/reports.dart
@@ -5,6 +5,47 @@ class Reports extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final data = List.generate(
+      10,
+          (index) => ReportData(
+        'Report $index',
+        'Description $index',
+        'Date $index',
+      ),
+    );
+
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('Reports'),
+      ),
+      body: Center(
+        child: DataTable(
+          columns: [
+            DataColumn(label: Text('Report')),
+            DataColumn(label: Text('Description')),
+            DataColumn(label: Text('Date')),
+          ],
+          rows: List.generate(
+            data.length,
+                (index) => DataRow(
+              cells: [
+                DataCell(Text(data[index].report)),
+                DataCell(Text(data[index].description)),
+                DataCell(Text(data[index].date)),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
+}
+
+class ReportData {
+  final String report;
+  final String description;
+  final String date;
+
+  ReportData(this.report, this.description, this.date);
 }
