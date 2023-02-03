7f9e78e13325521732c18415ed2ff0a25d458f7f
theAlphaGuardian <44818405+JosephLahiru@users.noreply.github.com>
2023-02-03 13:18:46 +0530

    Finished Basic splash screen

--- lib/screens/splash.dart
+++ lib/screens/splash.dart
@@ -1,19 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:mcms_app/screens/dashboard.dart';
 
-class Splash extends StatelessWidget {
+class Splash extends StatefulWidget {
   const Splash({Key? key}) : super(key: key);
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  void initState() {
+    super.initState();
+    _navigateToHome();
+  }
+
+  _navigateToHome() async {
+    await Future.delayed(const Duration(microseconds: 1500), () {});
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => Dashboard(),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Container(
-          child: const Text(
-            "Splash Screen",
-            style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-          ),
-        ),
-      ),
-    );
+    return const Placeholder();
   }
 }
