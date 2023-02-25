02f6534ecfa5e6f9ac275b36c2044e662d72feda
INFINITY <nirajnimantha@gmail.com>
2023-02-25 17:49:55 +0530

    modify reports

--- lib/screens/reports.dart
+++ lib/screens/reports.dart
@@ -1,48 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:mcms_app/assets/color.dart' as color;
 
 class Reports extends StatelessWidget {
   const Reports({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final data = List.generate(
-      10,
-          (index) => ReportData(
-        'Report $index',
-        'Description $index',
-        'Date $index',
-      ),
-    );
+    final colorScheme = Theme.of(context).colorScheme;
+    final textColor = colorScheme.brightness == Brightness.dark ? Colors.white : Colors.black;
 
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text('Reports'),
-      ),
-      body: Container(
-        padding: const EdgeInsets.only(),
-        child: Column(
-          children: [
-            Container(
-              width: MediaQuery.of(context).size.width,
-              height: 200,
-              //add red color square
-              decoration: BoxDecoration(
-                color: Colors.red,
-                borderRadius: BorderRadius.circular(10),
+        body: Container(
+          padding: const EdgeInsets.only(top: 70.0, left: 30.0, right: 30.0),
+          child: Column(
+            children: [
+              Row (
+                children: [
+                  Text(
+                    "Reports",
+                    style: TextStyle(
+                      fontSize: 30.0,
+                      fontWeight: FontWeight.w700,
+                      // Use ColorScheme to change text color based on system color
+                      // Set text color to black by default
+                      color: textColor,
+                    ),
+                  ),
+                  Expanded(child: Container()),
+                  Icon(
+                    Icons.arrow_back_ios, color: color.AppColors.grey, size: 30.0,),
+                  Icon(
+                    Icons.calendar_today_outlined, color: color.AppColors.grey, size: 30.0,),
+                  Icon(
+                    Icons.arrow_forward_ios, color: color.AppColors.grey, size: 30.0,),
+                ],
               ),
-            )
-          ],
-        ),
-      )
+            ],
+          ),
+        )
     );
   }
-}
-
-class ReportData {
-  final String report;
-  final String description;
-  final String date;
-
-  ReportData(this.report, this.description, this.date);
 }
