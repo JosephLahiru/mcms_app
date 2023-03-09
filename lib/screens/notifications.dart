b19262285975423749d8c2ab3cf629baf912085a
theAlphaGuardian <44818405+JosephLahiru@users.noreply.github.com>
2023-03-10 04:31:43 +0530

    Finished getting Notification from mysql

--- lib/screens/notifications.dart
+++ lib/screens/notifications.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import '../model/notifi.dart';
 
@@ -9,15 +11,14 @@ class Notifications extends StatefulWidget {
 }
 
 class _NotificationsState extends State<Notifications> {
-  List<Notifi> notifications = getNotifi();
+  Future<List<Notifi>> notifiFuture = getNotifi();
 
-  static List<Notifi> getNotifi() {
-    const data = [
-      {"body": "Yo Was-sup?"},
-      {"body": "How Are you?"},
-    ];
+  static Future<List<Notifi>> getNotifi() async {
+    const url = 'http://158.101.10.103/get_notification';
+    final response = await http.get(Uri.parse(url));
 
-    return data.map<Notifi>(Notifi.fromJson).toList();
+    final body = json.decode(response.body);
+    return body.map<Notifi>(Notifi.fromJson).toList();
   }
 
   @override
@@ -27,11 +28,26 @@ class _NotificationsState extends State<Notifications> {
           centerTitle: true,
         ),
         body: Center(
-          child: buildNotifi(notifications),
+          child: FutureBuilder<List<Notifi>>(
+            future: notifiFuture,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting)
+                return const CircularProgressIndicator();
+              else if (snapshot.hasError) {
+                return Text("Got an error: ${snapshot.error}");
+              } else if (snapshot.hasData) {
+                final notifications = snapshot.data!;
+
+                return buildNotifi(notifications);
+              } else {
+                return const Text('No notifications found');
+              }
+            },
+          ),
         ),
       );
 
-  Widget buildNotifi(List<Notifi> notification) => ListView.builder(
+  Widget buildNotifi(List<Notifi> notifications) => ListView.builder(
         itemCount: notifications.length,
         itemBuilder: (context, index) {
           final notification = notifications[index];
