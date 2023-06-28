5cbaa6ce11052d82d72c29ff440f3b373dcd3982
theAlphaGuardian <joseph.rasanjana1@gmail.com>
2023-06-28 18:27:18 +0530

    Added unseen Notification count

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mcms_app/screens/reports.dart';
+import 'package:http/http.dart' as http;
 
 import 'inventory_details.dart';
 // import 'notifications.dart';
@@ -13,6 +14,16 @@ class Dashboard extends StatelessWidget {
   List<String> images = ["notification.png", "Inventory.png", "report.png"];
   String drName = "Harsha";
   String drPic = "assets/images/dr.png";
+
+  Future<int> getUnseenNotificationsCount() async {
+    final response = await http.get(Uri.parse('http://158.101.10.103/get_seen_count'));
+
+    if (response.statusCode == 200) {
+      return int.parse(response.body);
+    } else {
+      throw Exception('Failed to load unseen notifications count');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -115,21 +126,50 @@ class Dashboard extends StatelessWidget {
                       borderRadius: BorderRadius.circular(16.0),
                     ),
                     elevation: 6,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
+                    child: Stack(
                       children: [
-                        Image.asset(
-                          "assets/images/${images[index]}",
-                          height: 128.0,
-                          width: 128.0,
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(top: 4.0),
-                          child: Text(
-                            views[index],
-                            style: TextStyle(color: Colors.white, fontSize: 25),
+                        Center( // Center the Column
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Image.asset(
+                                "assets/images/${images[index]}",
+                                height: 128.0,
+                                width: 128.0,
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(top: 4.0),
+                                child: Text(
+                                  views[index],
+                                  style: TextStyle(color: Colors.white, fontSize: 25),
+                                ),
+                              ),
+                            ],
                           ),
                         ),
+                        if (index == 0)
+                          FutureBuilder<int>(
+                            future: getUnseenNotificationsCount(),
+                            builder: (context, snapshot) {
+                              if (snapshot.connectionState == ConnectionState.waiting) {
+                                return CircularProgressIndicator();
+                              } else if (snapshot.hasError) {
+                                return Icon(Icons.error);
+                              } else {
+                                return Positioned(
+                                  top: 8.0,
+                                  right: 8.0,
+                                  child: CircleAvatar(
+                                    backgroundColor: Colors.red,
+                                    child: Text(
+                                      '${snapshot.data}',
+                                      style: TextStyle(color: Colors.white),
+                                    ),
+                                  ),
+                                );
+                              }
+                            },
+                          ),
                       ],
                     ),
                   ),
